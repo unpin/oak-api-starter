6903fdd6b1589a54b3e007d8fafc982480b0f740
German Bilalov <pinncho@gmail.com>
2022-08-16 16:51:59 +0600

    refactor: use toString to convert decimal to hex

--- utils/generateHexString.ts
+++ utils/generateHexString.ts
@@ -1,6 +1,4 @@
-const VALUES = "0123456789abcdef";
-
 export function generateHexString(n = 16) {
-  return Array.from(Array(n), () => VALUES[Math.floor(Math.random() * 16)])
+  return Array.from(Array(n), () => Math.floor(Math.random() * 16).toString(16))
     .join("");
 }
