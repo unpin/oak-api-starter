dfa150addfff263b010eb3ca6d9de20e45e696b1
German Bilalov <pinncho@gmail.com>
2022-08-17 22:47:05 +0600

    feat: check if auth token starts with `Bearer`

--- middleware/isAuth.ts
+++ middleware/isAuth.ts
@@ -4,8 +4,11 @@ import { verify } from "../common/jwt.ts";
 import { JWT_CRYPTO_KEY } from "../common/config.ts";
 
 export async function isAuth(ctx: Context, next: () => Promise<unknown>) {
-  const auth = ctx.request.headers.get("Authorization");
-  const token = auth?.split(" ")[1];
+  const authHeader = ctx.request.headers.get("Authorization");
+  let token: string | null = null;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  }
   if (!token) {
     throw createHttpError(
       Status.Unauthorized,
@@ -14,6 +17,7 @@ export async function isAuth(ctx: Context, next: () => Promise<unknown>) {
   }
   try {
     ctx.state.user = await verify(token, JWT_CRYPTO_KEY);
+    // TODO Check if user exists in the database
   } catch {
     throw createHttpError(
       Status.Unauthorized,
