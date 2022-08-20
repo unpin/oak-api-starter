4e6960dc9c6f122242ba693f17ed026c84a63a4a
German Bilalov <pinncho@gmail.com>
2022-08-20 20:04:59 +0600

    feat: rewrite tests using Deno's steps API

--- tests/api/auth.test.ts
+++ tests/api/auth.test.ts
@@ -1,52 +1,76 @@
 import { Status } from "std/http/http_status.ts";
 import { assertExists } from "std/testing/asserts.ts";
-import { afterAll, beforeAll, describe, it } from "std/testing/bdd.ts";
 import { Fetch } from "superfetch";
-import { Auth, Users } from "./config/routes.ts";
+import { createRandomUser } from "../../utils/randomUser.ts";
+import { Auth } from "./config/routes.ts";
 
-const userData = {
-  name: "Example",
-  email: "example@example.com",
-  password: "******",
-};
+Deno.test("user registration", async ({ step }) => {
+  const randomUser = createRandomUser();
 
-const deleteUser = async () => {
-  const response = await Fetch.delete(Users.deleteMany)
-    .send({ email: userData.email });
-  response.close();
-};
-
-beforeAll(deleteUser);
-afterAll(deleteUser);
-
-describe("POST /auth/signup", () => {
-  it("should register a new user", async () => {
-    const response = await Fetch.post(Auth.signup).send(userData);
+  await step("should return status 201 and a token", async () => {
+    const response = await Fetch.post(Auth.signup).send(randomUser);
     response.expect(Status.Created);
+    const { token } = await response.json;
+    assertExists(token);
     response.close();
   });
 
-  it("should return 409 if user with the email already exists", async () => {
-    const response = await Fetch.post(Auth.signup).send(userData);
+  await step("should return status 409 when email already exists", async () => {
+    const response = await Fetch.post(Auth.signup).send(randomUser);
     response.expect(Status.Conflict);
     response.close();
   });
+
+  await step(
+    "should return status 400 when required fields are not provided",
+    async () => {
+      const response = await Fetch.post(Auth.signup).send({});
+      response.expect(Status.BadRequest);
+      response.close();
+    },
+  );
+
+  await step(
+    "should return status 400 when if password is less than 6 characters long",
+    async () => {
+      const response = await Fetch.post(Auth.signup).send({
+        ...createRandomUser(),
+        password: "12345",
+      });
+      response.expect(Status.BadRequest);
+      response.close();
+    },
+  );
 });
 
-describe("POST /auth/signin", () => {
-  it("should respond with 401 if email or password is wrong", async () => {
-    const response = await Fetch.post(Auth.signin)
-      .send({ email: "wrong@email.com", password: "wrong" });
-    response.expect(Status.Unauthorized);
+Deno.test("user signing in", async ({ step }) => {
+  await step("should return a 200 status and JWT token", async ({ step }) => {
+    const randomUser = createRandomUser();
+    let response = await Fetch.post(Auth.signup).send(randomUser);
+    response.expect(Status.Created);
     response.close();
-  });
-
-  it("should sign in user responding with JWT token", async () => {
-    const response = await Fetch.post(Auth.signin)
-      .send({ email: userData.email, password: userData.password });
+    response = await Fetch.post(Auth.signin).send(randomUser);
     response.expect(Status.OK);
-    const json = await response.json;
-    assertExists(json);
-    assertExists(json.token);
+    const { token } = await response.json;
+    assertExists(token);
+    await step("should return 401 if password is incorrect", async () => {
+      const response = await Fetch.post(Auth.signin)
+        .send({ email: randomUser.email, password: "wrong" });
+      response.expect(Status.Unauthorized);
+      response.close();
+    });
   });
+});
+
+Deno.test("should return a 204 and delete user", async () => {
+  const { name, email, password } = createRandomUser();
+  let response = await Fetch.post(Auth.signup)
+    .send({ email, name, password });
+  response.expect(Status.Created);
+  const { token } = await response.json;
+  assertExists(token);
+  response = await Fetch.delete(Auth.deleteAccount)
+    .set("Authorization", `Bearer ${token}`)
+    .send({ password });
+  response.expect(Status.NoContent);
 });
