2f91b92da5c8f41e9f91dd64cdb194f8da57446f
German Bilalov <pinncho@gmail.com>
2022-08-26 20:00:57 +0600

    chore: refactor validateObjectId middleware

--- middleware/validateObjectId.ts
+++ middleware/validateObjectId.ts
@@ -1,4 +1,3 @@
-import { LOGGER } from "../common/Logger.ts";
 import { createHttpError, RouteParams, RouterContext } from "oak";
 import { isObjectIdValid } from "../utils/isObjectIdValid.ts";
 import { Status } from "oak";
@@ -8,12 +7,15 @@ export function validateObjectId<T extends string>(params: string[]) {
     ctx: RouterContext<T, RouteParams<T>>,
     next: () => Promise<unknown>,
   ) {
-    if (params.every((id) => isObjectIdValid(ctx.params[id]))) {
-      LOGGER.debug("ObjectID is valid");
-      await next();
-    } else {
-      LOGGER.debug("ObjectID is NOT valid");
-      throw createHttpError(Status.BadRequest, "Invalid ObjectIds");
+    const invalidIDs: string[] = params.filter((param) =>
+      !isObjectIdValid(ctx.params[param])
+    );
+    if (invalidIDs.length) {
+      throw createHttpError(
+        Status.BadRequest,
+        `Invalid ObjectId(s) (${invalidIDs.join(", ")})`,
+      );
     }
+    await next();
   };
 }
