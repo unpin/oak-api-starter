2b956f856c88836acc9153542f3c2001bc961060
German Bilalov <pinncho@gmail.com>
2022-09-04 12:54:19 +0600

    debug: disable file logger

--- common/Logger.ts
+++ common/Logger.ts
@@ -1,30 +1,30 @@
-import { ensureDir, ensureDirSync } from "std/fs/mod.ts";
+// import { ensureDir, ensureDirSync } from "std/fs/mod.ts";
 import { getLogger, handlers, LevelName, setup } from "std/log/mod.ts";
 import { LOGGING_LEVEL } from "../common/config.ts";
 
 const LEVEL = LOGGING_LEVEL as LevelName || "NOTSET";
 // File Logger Configuration
-const FILENAME = "./logs/log.txt";
-const MAX_BYTES = 1024 * 1024 * 5;
-const MAX_BACKUP_COUNT = 3;
-const FORMATTER = "[{levelName}] {datetime} {msg}";
+// const FILENAME = "./logs/log.txt";
+// const MAX_BYTES = 1024 * 1024 * 5;
+// const MAX_BACKUP_COUNT = 3;
+// const FORMATTER = "[{levelName}] {datetime} {msg}";
 
-await ensureDir(Deno.cwd() + "./logs");
+// await ensureDir(Deno.cwd() + "./logs");
 
 await setup({
   handlers: {
     console: new handlers.ConsoleHandler("DEBUG"),
-    file: new handlers.RotatingFileHandler("ERROR", {
+    /* file: new handlers.RotatingFileHandler("ERROR", {
       filename: FILENAME,
       formatter: FORMATTER,
       maxBytes: MAX_BYTES,
       maxBackupCount: MAX_BACKUP_COUNT,
-    }),
+    }),*/
   },
   loggers: {
     default: {
       level: LEVEL,
-      handlers: ["console", "file"],
+      handlers: ["console" /* , "file" */],
     },
   },
 });
