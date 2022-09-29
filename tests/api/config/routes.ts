21a01bb2ef7fcfd5e8f44b5b6255f8a2bb346c64
German Bilalov <pinncho@gmail.com>
2022-09-29 20:49:28 +0600

    fix: update URL for deleting user

--- tests/api/config/routes.ts
+++ tests/api/config/routes.ts
@@ -3,7 +3,7 @@ const BASE_URL = "http://localhost:8000";
 export const Auth = {
   signup: `${BASE_URL}/auth/signup`,
   signin: `${BASE_URL}/auth/signin`,
-  deleteAccount: `${BASE_URL}/delete-account`,
+  deleteAccount: `${BASE_URL}/account/delete`,
 };
 
 export const Users = {
